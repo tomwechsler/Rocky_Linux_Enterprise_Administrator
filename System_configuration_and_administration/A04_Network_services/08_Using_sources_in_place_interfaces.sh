aae86ea3d0aa1c71038ab255cc9f26de4c729e30
Tom Wechsler <tom.wechsler@outlook.com>
2025-03-28 06:44:08 +0100

    New Files

---
+++ System_configuration_and_administration/A04_Network_services/08_Using_sources_in_place_interfaces.sh
@@ -0,0 +1,25 @@
+#Working on Rocky01
+
+#Enter the root shell
+sudo -i
+
+#Remove the http service
+firewall-cmd --remove-service=http
+
+#Curl from the remote host
+curl 192.168.56.101
+
+#Add the http service to the internal zone
+firewall-cmd --add-service=http --zone=internal
+
+#List the internal zone
+firewall-cmd --list-all --zone=internal
+
+#Add a source to the internal zone (the zone comes active)
+firewall-cmd --add-source=192.168.56.0/24 --zone=internal
+
+#Curl from the remote host
+curl 192.168.56.101
+
+#Write rules to the permanent configuration
+firewall-cmd --runtime-to-permanent
---
+++ System_configuration_and_administration/A04_Network_services/10_Configuring_fail2ban.sh
@@ -0,0 +1,38 @@
+#Working on Rocky01
+
+#First check the log file for the sshd service
+sudo journalctl --unit sshd
+
+#Check the log file for failed sshd logins
+sudo journalctl --unit sshd | grep "Failed password"
+
+#The log entries from sshd since 14:00
+sudo journalctl --unit sshd --since "14:00"
+
+#We need the epel repository to install fail2ban
+sudo dnf install epel-release -y
+
+#Install fail2ban
+sudo dnf install fail2ban -y
+
+#Create a fail2ban sshd configuration file
+sudo vim /etc/fail2ban/jail.d/sshd.conf
+
+[DEFAULT]
+bantime = 72h
+findtime = 10m
+maxretry = 3
+backend = auto
+[sshd]
+enabled = true
+
+#Save and exit
+
+#Start the fail2ban service
+sudo systemctl enable --now fail2ban
+
+#Check the status of the fail2ban service
+sudo systemctl status fail2ban
+
+#Check with the fail2ban client
+sudo fail2ban-client status sshd
