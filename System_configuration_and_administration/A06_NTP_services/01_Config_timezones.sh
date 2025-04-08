30773a17ab8ffa1d40b8553b5b1a75b954a164df
Tom Wechsler <tom.wechsler@outlook.com>
2025-04-08 06:50:47 +0200

    New files

---
+++ System_configuration_and_administration/A06_NTP_services/01_Config_timezones.sh
@@ -0,0 +1,28 @@
+#Working on Rocky01
+
+#Open a root shell
+sudo -i
+
+#List the available chrony packages
+dnf list chrony
+
+#Check the status of the chronyd service
+systemctl status chronyd
+
+#Display the current system time and timezone
+timedatectl
+
+#Set the system clock to use UTC instead of the local time
+timedatectl set-local-rtc 0
+
+#Display the updated system time and timezone
+timedatectl
+
+#Set the system timezone to Europe/Zurich
+timedatectl set-timezone Europe/Zurich
+
+#If we get an error, create a symbolic link between the timezone file and the localtime file
+ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+
+#Display the final system time and timezone
+timedatectl
---
+++ System_configuration_and_administration/A06_NTP_services/02_Managing_chronyd.sh
@@ -0,0 +1,28 @@
+#Working on Rocky01
+
+#Open a root shell
+sudo -i
+
+#Display the status of all services
+systemctl status
+
+#Display the status of the chronyd service
+systemctl status chronyd
+
+#Disable and stop the chronyd service
+systemctl disable --now chronyd
+
+#Display the status of the chronyd service
+systemctl status chronyd
+
+#Enable and start the chronyd service
+systemctl enable --now chronyd
+
+#Display the contents of the chronyd service unit file
+systemctl cat chronyd
+
+#Edit the chronyd service unit file
+systemctl edit chronyd --full
+
+#Display the contents of the chronyd service unit file
+systemctl cat chronyd
