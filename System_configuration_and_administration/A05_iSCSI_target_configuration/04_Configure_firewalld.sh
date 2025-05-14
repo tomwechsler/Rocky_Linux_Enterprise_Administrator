f640c2ddde0395531f38a978b12a1a8eb2702e05
Tom Wechsler <tom.wechsler@outlook.com>
2025-05-14 10:10:45 +0200

    lines edited

--- System_configuration_and_administration/A05_iSCSI_target_configuration/04_Configure_firewalld.sh
+++ System_configuration_and_administration/A05_iSCSI_target_configuration/04_Configure_firewalld.sh
@@ -9,6 +9,12 @@ sudo netstat -at
 #Display all active TCP connections with numeric addresses and ports
 sudo netstat -atn
 
+#Display all active TCP connections with numeric addresses and ports (the new way)
+sudo ss -ntl
+
+#Display all active TCP connections with numeric addresses, ports and process names
+sudo ss -ntlp
+
 #Permanently allow the iSCSI target service through the firewall
 sudo firewall-cmd --permanent --add-service=iscsi-target
 
